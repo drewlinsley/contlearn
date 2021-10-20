471ac2c718847fe01c1e0bcf02f05a121afd18e0
Drew Linsley <drewlinsley@gmail.com>
2021-10-20 19:43:44 -0400

    Updated defaults for debug.

--- create_vm.sh
+++ create_vm.sh
@@ -6,8 +6,7 @@ ZONE=us-central1-f  # europe-west4-a
 TPU=v3-8
 
 
-gcloud compute instances delete $TPUNAME --zone=$ZONE --quiet
-
+gcloud alpha compute tpus tpu-vm delete $TPUNAME --zone=$ZONE --quiet
 gcloud alpha compute tpus tpu-vm create $TPUNAME \
 --zone=$ZONE \
 --accelerator-type=v3-8 \
@@ -17,5 +16,5 @@ gcloud alpha compute tpus tpu-vm create $TPUNAME \
 # gcloud alpha compute tpus tpu-vm ssh $TPUNAME --zone $ZONE \
 #   --command "git clone https://github.com/drewlinsley/contlearn.git && cd contlearn && pip3 -r install requirements.txt && bash tpu_vm.sh"
 gcloud alpha compute tpus tpu-vm ssh $TPUNAME --zone $ZONE \
-  --command "git clone https://github.com/drewlinsley/contlearn.git && cd contlearn && pip3 install -r requirements.txt"
+  --command "git clone https://github.com/drewlinsley/contlearn.git && cd contlearn && pip install -r requirements.txt && git checkout gcp"
 gcloud alpha compute tpus tpu-vm ssh $TPUNAME --zone $ZONE
