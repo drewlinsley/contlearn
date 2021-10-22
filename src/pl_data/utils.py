05abec3c5892712fcea8a041f387db61b0b4764b
Drew Linsley <drewlinsley@gmail.com>
2021-10-22 11:58:10 -0400

    Updated tfrecord reader.

--- src/pl_data/utils.py
+++ src/pl_data/utils.py
@@ -1,6 +1,7 @@
 import os
 import tensorflow as tf
 import sys
+os.environ['NO_GCE_CHECK'] = 'true'
 
 
 def _parse_image_function(example_proto):
@@ -8,29 +9,27 @@ def _parse_image_function(example_proto):
     return tf.io.parse_single_example(example_proto, image_feature_description)
 
 
-output_dir = "npz_files"
-# filename = "clickme_train"
-filename = sys.argv[1]
-os.makedirs(output_dir, exist_ok=True)
-os.makedirs(os.path.join(output_dir, filename), exist_ok=True)
+output_dir = "gs://serrelab/connectomics/npzs/celltype/train"
+filename = "gs://serrelab/connectomics/tfrecords/celltype/cell_type_10_64_15.tfrecords_train.tfrecords"
 
-raw_dataset = tf.data.TFRecordDataset(os.path.join("archive", "{}.tfrecords".format(filename)))
+raw_dataset = tf.data.TFRecordDataset(os.path.join(filename))
 image_feature_description = {
-    'label': tf.io.FixedLenFeature([], tf.int64),
-    'click_count': tf.io.FixedLenFeature([], tf.int64),
-    'image': tf.io.FixedLenFeature([], tf.string),
-    'heatmap': tf.io.FixedLenFeature([], tf.string),
+    'volume': tf.io.FixedLenFeature([], tf.string),
+    'label': tf.io.FixedLenFeature([], tf.string),
 }
 parsed_image_dataset = raw_dataset.map(_parse_image_function)
 for idx, image_features in enumerate(parsed_image_dataset):
-    image_bytes = tf.reshape(image_features['image'], shape=[])
-    hm_bytes = tf.reshape(image_features['heatmap'], shape=[])
-    label = image_features["label"].numpy()
-    clicks = image_features["click_count"].numpy()
-    image = tf.io.decode_raw(image_bytes, tf.float32)
-    image = tf.reshape(image, [256, 256, 3])
-    heatmap = tf.io.decode_raw(hm_bytes, tf.float32)
-    heatmap = tf.reshape(heatmap, [256, 256])
+    volume_bytes = tf.reshape(image_features['volume'], shape=[])
+    label_bytes = tf.reshape(image_features['label'], shape=[])
+    volume = tf.io.decode_raw(volume_bytes, tf.float32)
+    volume = tf.reshape(volume, [64, 128, 128, 2])
+    label = tf.io.decode_raw(label_bytes, tf.float32)
+    label = tf.reshape(label, [64, 128, 128, 2])
+
+    # Transpose now for pytorch
+    volume = volume.transpose(3, 0, 1, 2)
+    label = label.transpose(3, 0, 1, 2)
+
     path = os.path.join(output_dir, filename, "{}".format(idx))
-    np.savez(path, image=image, heatmap=heatmap, clicks=clicks, label=label)
+    np.savez(path, volume=volume, label=label)
 
