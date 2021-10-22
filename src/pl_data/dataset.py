63bd15e64ff48f7e2a157b6ab60f5390a8b4714d
Drew Linsley <drewlinsley@gmail.com>
2021-10-22 13:51:12 -0400

    Added npz loading.

--- src/pl_data/dataset.py
+++ src/pl_data/dataset.py
@@ -8,6 +8,7 @@ import csv
 from torchvision import transforms
 import tensorflow as tf  # for reading TFRecord Dataset
 import tensorflow_datasets as tfds  # for making tf.data.Dataset to return numpy arrays
+import numpy as np
 
 
 def load_image(directory):
