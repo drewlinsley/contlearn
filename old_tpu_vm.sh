2b650684e6f91a34ec21e11dcf0e1955201a7d9e
Drew Linsley <drewlinsley@gmail.com>
2021-10-29 15:15:02 -0400

    Working on old tpus.

--- old_tpu_vm.sh
+++ old_tpu_vm.sh
@@ -1,4 +1,11 @@
 
+conda create --name gcp -y
+conda activate gcp
+conda install pathlib
+cp netrc ../.netrc
+pip install -r requirements.txt
+
+
 export TPU_IP_ADDRESS=$(cat tpuip.txt)  # You could get the IP Address in the GCP TPUs section
 export XRT_TPU_CONFIG="tpu_worker;0;$TPU_IP_ADDRESS:8470"
 
