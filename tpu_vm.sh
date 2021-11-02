abc6f5556ceb54fe10da9faa45daeb30f9797398
Drew Linsley <drewlinsley@gmail.com>
2021-11-01 22:56:52 -0400

    Added tpu-speccific configs.

--- tpu_vm.sh
+++ tpu_vm.sh
@@ -3,4 +3,4 @@
 export XRT_TPU_CONFIG="localservice;0;localhost:51011"
 
 # pip3 install -r requirements.txts
-python3 run.py --config-name=celltype_gcp.yaml -m model.name=UNet3D
+python3 run.py --config-name=celltype_gcp_tpu_8.yaml -m model.name=UNet3D
