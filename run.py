0b1de3304cdcd2527ce9790bb692dd34056826a8
Drew Linsley <drewlinsley@gmail.com>
2021-11-04 18:44:15 -0400

    Updated.

--- run.py
+++ run.py
@@ -128,7 +128,7 @@ def run(cfg: DictConfig) -> None:
         #callbacks=callbacks,
         # deterministic=cfg.train.deterministic,
         val_check_interval=cfg.logging.val_check_interval,
-        log_every_n_steps=10,
+        log_every_n_steps=2,
         progress_bar_refresh_rate=cfg.logging.progress_bar_refresh_rate,
         #auto_select_gpus=True,
         # benchmark=True,
