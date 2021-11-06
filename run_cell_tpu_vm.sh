ac5463d0b3edb3ef050efd29af44f6166e6a3131
Drew Linsley <drewlinsley@gmail.com>
2021-11-06 16:21:49 -0400

    Updates.

--- run_cell_tpu_vm.sh
+++ run_cell_tpu_vm.sh
@@ -3,4 +3,6 @@
 export XRT_TPU_CONFIG="localservice;0;localhost:51011"
 
 # pip3 install -r requirements.txts
+cp netrc ../.netrc
 python3 run.py --config-name=celltype_gcp_tpu_1.yaml -m model.name=UNet3D
+gsutil cp -r results/* gs://serrelab/connectomics/results/
