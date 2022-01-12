f9f443f085ed1a2238f6b0f032f299b681a96513
Drew Linsley <drewlinsley@gmail.com>
2022-01-12 16:57:26 -0500

    Updated old tpu run script.

--- run_cell_old_tpu.sh
+++ run_cell_old_tpu.sh
@@ -9,4 +9,4 @@ export TPU_IP_ADDRESS=$(cat tpuip.txt)  # You could get the IP Address in the GC
 export XRT_TPU_CONFIG="tpu_worker;0;$TPU_IP_ADDRESS:8470"
 
 # pip3 install -r requirements.txts
-python3 run.py --config-name=celltype_gcp.yaml -m model.name=UNet3D
+python3 run.py --config-name=celltype_gcp_tpu_1.yaml
