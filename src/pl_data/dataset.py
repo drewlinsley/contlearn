1229205d84264e7529cc2ee5cc4df1d5b7a8eccd
Drew Linsley <drewlinsley@gmail.com>
2022-01-17 15:45:48 -0500

    Casting to single.

--- src/pl_data/dataset.py
+++ src/pl_data/dataset.py
@@ -132,7 +132,7 @@ class Volumetric(Dataset):
         # self.len = None  # TESTING AUTO-COUNT
         self.augmentations = [
             # {"randomcrop": self.shape},
-            {"randomselection": None},
+            {"random_selection": None},
             # {"randomrotate": [(1, 2), (1, 3), (2, 3)]},  # noqa Axes to rotate -- this only works for isotropic voxels
             # {"randomrotate": [(2, 3)]},  # Axes to rotate
             # {"randomflip": [1, 2, 3]},  # Axes to rotate
