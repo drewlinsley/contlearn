7f7a89e10c8323ae9c6cc126d68cf30b4fccdbae
Drew Linsley <drewlinsley@gmail.com>
2022-01-18 15:23:21 -0500

    Casting to single.

--- src/pl_modules/model.py
+++ src/pl_modules/model.py
@@ -189,7 +189,6 @@ class MyModel(pl.LightningModule):
             # print("logits: {}".format(output_element["logits"].shape))
 
             mid = output_element["image"].shape[1] // 2  # midpoint on z-axis
-            print("loop start")
 
             if self.cfg.model.plot_argmax:
                 gt = output_element["y_true"][mid].argmax(dim=0)[None]
@@ -199,33 +198,29 @@ class MyModel(pl.LightningModule):
             if len(output_element["image"]) == 2:
                 input_img = output_element["image"][0, mid][None]
                 input_seg = output_element["image"][1, mid][None]
-                print("creating image")
                 rendered_image = torchvision.utils.make_grid(
                     torch.cat([
                         input_img.float(),
                         input_seg.float(),
                         gt.float(),
-                        output_seg.float()], 0).detach(),
+                        output_seg.float()],
+                        0).detach(),
                     nrow=4,
                     scale_each=True
                 )
                 caption = f"image____mem____GT____output"
             else:
+                raise NotImplementedError("Needs to be tested.")
                 input_img = output_element["image"][0, mid][None]
                 rendered_image = render_images(
-                    [input_img, gt, output_seg],
+                    [input_img.float(), gt.float(), output_seg.float()],
                     autoshow=False, nrow=1)
                 caption = f"image____GT____output"
-            print("writing image")
-            images.append(
-                wandb.Image(
-                    rendered_image,
-                    caption=caption,
-                )
-            )
-            print("loop end")
+            images.append(rendered_image)
+        print("loop end")
+        wandb_image = [wandb.Image(x, caption=caption) for x in images]
         self.logger.experiment.log(
-            {"Validation Images": images})
+            {"Validation Images": wandb_image})
 
     def test_epoch_end(self, outputs: List[Any]) -> None:
         # batch_size = self.cfg.data.datamodule.batch_size.test
