33e23a660e0273674b22566aca181357724c466c
Drew Linsley <drewlinsley@gmail.com>
2022-01-19 23:01:41 -0500

    Fixed bug.

--- src/pl_modules/model.py
+++ src/pl_modules/model.py
@@ -99,11 +99,11 @@ class MyModel(pl.LightningModule):
         else:
             # loss = self.loss(logits, y, self.loss_weights)
             # loss = self.loss(logits, y, maxval=self.maxval)
-            onehot_y = F.one_hot(
-                y.squeeze(1).to(torch.int64),
-                self.maxval).to(
-                logits.dtype).permute(0, 4, 1, 2, 3)
-            loss = self.loss(logits, onehot_y)
+            # onehot_y = F.one_hot(
+            #     y.squeeze(1).to(torch.int64),
+            #     self.maxval).to(
+            #     logits.dtype).permute(0, 4, 1, 2, 3)
+            loss = self.loss(logits, y)  # onehot_y)
         return {
             "logits": logits.detach(),
             "loss": loss,
