c6c3f8b905525bf3d70604acf79cffff4956f8ad
Drew Linsley <drewlinsley@gmail.com>
2022-01-26 15:24:18 -0500

    Changed loss function.

--- src/pl_data/datamanager.py
+++ src/pl_data/datamanager.py
@@ -239,9 +239,8 @@ class GetData():
                         for l in uni_labels:
                             if l not in self.keep_labels.keys():
                                 remap_to_0[l] = 0
-                        import pdb;pdb.set_trace()
                         keep_labels = self.keep_labels
-                        keep_labels = remap_to_0.update(keep_labels)
+                        remap_to_0.update(keep_labels)
                         label = fastremap.remap(label, keep_labels)
                     import pdb;pdb.set_trace()
                     # Then get the dataset images
