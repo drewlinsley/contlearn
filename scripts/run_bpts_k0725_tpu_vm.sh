1ba8e2c5daece7917127aa1986a83fb6351cee0c
Drew Linsley <drewlinsley@gmail.com>
2022-01-31 12:40:31 -0500

    Added new configs.

---
+++ scripts/run_bpts_k0725_tpu_vm.sh
@@ -0,0 +1,14 @@
+
+
+export XRT_TPU_CONFIG="localservice;0;localhost:51011"
+
+cp netrc ../.netrc
+# export PT_XLA_DEBUG=1
+# export USE_TORCH=ON
+
+unset LD_PRELOAD
+
+# python run.py --config-name=celltype_gcp_tpu_1.yaml
+python run.py --config-name=k0725_bpts_tpu.yaml
+# gsutil -m cp -r results/* gs://serrelab/connectomics/results/
+# gsutil -m cp -r results/*/*/*/*/results/*/*/*/*/* gs://serrelab/connectomics/results/
---
+++ scripts/run_tpu_vm_job.sh
@@ -0,0 +1,16 @@
+
+
+export XRT_TPU_CONFIG="localservice;0;localhost:51011"
+
+CONFIG=$1
+
+cp netrc ../.netrc
+# export PT_XLA_DEBUG=1
+# export USE_TORCH=ON
+
+unset LD_PRELOAD
+
+# python run.py --config-name=celltype_gcp_tpu_1.yaml
+python run.py --config-name=$CONFIG
+# gsutil -m cp -r results/* gs://serrelab/connectomics/results/
+# gsutil -m cp -r results/*/*/*/*/results/*/*/*/*/* gs://serrelab/connectomics/results/
