b5a79fe31836ceba9a8fbf61aa66fe7d4a4af3f9
Drew Linsley <drewlinsley@gmail.com>
2022-02-07 22:02:43 -0500

    Updated baseline for WQ synapses tpu.

--- login_tpuvm.sh
+++ login_tpuvm.sh
@@ -1,14 +1,20 @@
-ZONE=us-central1-f  # europe-west4-a us-east1-d
+# ZONE=us-central1-a  # europe-west4-a us-east1-d
 # ZONE=europe-west4-a  #  us-east1-d
 TPU=v3-8  # 8
 
 TPUNAME=$1
+ZONE=$2
 
 if [ -z "$TPUNAME" ]
 then
   echo "Enter the name of your tpu on GCP:"
   read TPUNAME
   # pytorch-tpu1, muller-tpu-1
+fi
+
+if [ -z "$ZONE" ]
+then
+  ZONE=us-central1-a
 fi
 
 gcloud alpha compute tpus tpu-vm ssh $TPUNAME --zone $ZONE  --ssh-flag="-X"
