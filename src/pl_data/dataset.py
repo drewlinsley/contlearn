683772c6daee9dbe09fd1c535fad19345f91567a
Drew Linsley <drewlinsley@gmail.com>
2022-02-20 11:58:24 -0500

    Added wandb creds.

--- src/pl_data/dataset.py
+++ src/pl_data/dataset.py
@@ -60,6 +60,8 @@ class CIFAR10(Dataset):
 
     def __getitem__(self, index: int):
         img, label = self.dataset[index]
+        img = F.to_tensor(img)
+        # label = F.to_tensor(label)
         return img, label
 
     def __repr__(self) -> str:
