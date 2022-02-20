a835d56b7ef88a0c17072d0cfa0329017c931ac2
Drew Linsley <drewlinsley@gmail.com>
2022-02-20 11:48:41 -0500

    Added wandb creds.

--- src/pl_data/dataset.py
+++ src/pl_data/dataset.py
@@ -6,6 +6,7 @@ from os.path import isfile, join
 from PIL import Image
 import csv
 from torchvision import transforms
+from pl_bolts.datamodules.cifar10_dataset import CIFAR10 as cifar10_data
 
 
 def load_image(directory):
@@ -48,7 +49,7 @@ class CIFAR10(Dataset):
         self.train = train
         self.transform = transform
 
-        self.dataset = CIFAR10(data_dir="data/", download=True)
+        self.dataset = cifar10_data(data_dir="data/", download=True)
         self.data_len = len(self.dataset)
 
     def __len__(self) -> int:
