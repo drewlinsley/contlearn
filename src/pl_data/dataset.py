3fc7d2e8bdff151c69df2b9df4c6c66817fc196a
Drew Linsley <drewlinsley@gmail.com>
2022-03-02 20:47:43 -0500

    Updated requirements.

--- src/pl_data/dataset.py
+++ src/pl_data/dataset.py
@@ -138,7 +138,7 @@ class Volumetric(Dataset):
             {"randomrotate": [(2, 3)]},  # Axes to rotate
             # {"randomflip": [1, 2, 3]},  # Axes to rotate
             {"pixel_contrast_brightness_2d": {"contrast_factor": 0.5, "brightness_factor": 0.5, "prob": 1., "skip": 0.3}},
-            {"misalign_and_missing": {"disp": (5, 30), "value": 1, "random": True}}
+            {"misalign_and_missing": {"disp": (5, 30), "value": 1, "random": True}},
             {"normalize_volume": [0, 255]},  # Min/max
             {"cast_label": torch.int}
             # {"normalize_volume_z": [150.4, 31.8]},  # Min/max
@@ -240,6 +240,8 @@ class VolumetricWarp(Dataset):
             {"randomrotate": [(2, 3)]},  # Axes to rotate
             # {"randomflip": [1, 2, 3]},  # Axes to rotate
             {"normalize_volume": [0, 255]},  # Min/max
+            {"pixel_contrast_brightness_2d": {"contrast_factor": 0.5, "brightness_factor": 0.5, "prob": 1., "skip": 0.3}},
+            {"misalign_and_missing": {"disp": (5, 30), "value": 1, "random": True}},
             {"warp": {"skip": 0.3, "do_twist": False, "rot_max": 15., "scale_max": 1.2}},  # Min/max
             {"cast_label": torch.int}
             # {"normalize_volume_z": [150.4, 31.8]},  # Min/max
